28fc1d9944a9fe7d9f78e401f6099d12529a69d4
ehsan-torabi <torabi782@gmail.com>
2023-10-11 08:53:40 +0330

    Create main form

---
+++ SortTwoArr.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int main(int argc, char const *argv[])
+{
+    const int n = 5;
+    int arr1[n] = {5, 4, 3, 2, 1};
+    int arr2[n] = {10, 9, 8, 7, 6};
+    int sortedArr[n * 2];
+
+    
+
+    for(int e : sortedArr){
+        std::cout << e << std::endl;
+    }
+
+    return 0;
+}
