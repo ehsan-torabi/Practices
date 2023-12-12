f9bac184d62ee7934efea8032d11227ef3b0f57c
ehsan-torabi <torabi782@gmail.com>
2023-12-12 17:09:38 +0330

    Implement Node class

--- Left-Child Right-Sibling.cpp
+++ Left-Child Right-Sibling.cpp
@@ -5,23 +5,81 @@ private:
     T value;
     Node<T> *leftChild;
     Node<T> *rightSibling;
+    Node<T> *parrent;
 
 public:
     Node(T val);
-    Node<T>* getChild();
-    Node<T>* getSibling();
-    T getValue(); 
+    Node<T> *getChild();
+    Node<T> *getSibling();
+    Node<T> *getParrent();
+    T getValue();
     bool isRoot();
+    bool isLeaf();
     void setValue(T val);
     void setChild(Node<T> *ch);
     void setRightsibling(Node<T> *rs);
-    ~Node();
+    void setParrent(Node<T> *p);
 };
 template <typename T>
 Node<T>::Node(T val)
 {
 }
 template <typename T>
-Node<T>::~Node()
+Node<T> *Node<T>::getChild()
 {
+    return leftChild;
+}
+
+template <typename T>
+Node<T> *Node<T>::getSibling()
+{
+    return rightSibling;
+}
+
+template <typename T>
+Node<T> *Node<T>::getParrent()
+{
+    return parrent;
+}
+
+template <typename T>
+T Node<T>::getValue()
+{
+    return value;
+}
+
+template <typename T>
+bool Node<T>::isRoot()
+{
+    return (parrent == nullptr);
+}
+
+template <typename T>
+bool Node<T>::isLeaf()
+{
+    return (leftChild == nullptr && rightSibling == nullptr);
+}
+
+template <typename T>
+void Node<T>::setValue(T value)
+{
+    this->value = value;
+}
+
+template <typename T>
+void Node<T>::setChild(Node<T> *ch)
+{
+    leftChild = ch;
+}
+
+template <typename T>
+void Node<T>::setRightsibling(Node<T> *rs)
+{
+    rightSibling = rs;
+}
+
+template <typename T>
+void Node<T>::setParrent(Node<T> *p)
+{
+    parrent = p;
 }
