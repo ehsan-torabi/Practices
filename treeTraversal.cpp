526921a6567649fae234ba962c3682bec0c13807
ehsan-torabi <torabi782@gmail.com>
2023-12-17 19:17:27 +0330

    complete commenting

--- treeTraversal.cpp
+++ treeTraversal.cpp
@@ -2,161 +2,139 @@
 #include <string>
 using namespace std;
 
+// Define the binary node template class
 template <typename T>
 class BinNode
 {
 private:
-    T value;
-    BinNode<T> *leftChild;
-    BinNode<T> *rightChild;
+    T value; // Value of the node
+    BinNode<T> *leftChild; // Pointer to the left child node
+    BinNode<T> *rightChild; // Pointer to the right child node
 
 public:
-    BinNode(T value);
-    BinNode<T> *getLeftChild() const;
-    BinNode<T> *getRightChild() const;
-    T getValue();
-    bool isLeaf();
-    void setLeftChild(BinNode<T> *lc);
-    void setRightChild(BinNode<T> *rc);
+    // Constructor
+    BinNode(T value) : value(value), leftChild(nullptr), rightChild(nullptr) {}
+
+    // Getter for the left child
+    BinNode<T> *getLeftChild() const { return leftChild; }
+
+    // Getter for the right child
+    BinNode<T> *getRightChild() const { return rightChild; }
+
+    // Getter for the value
+    T getValue() { return value; }
+
+    // Check if the node is a leaf node
+    bool isLeaf() { return (leftChild == nullptr && rightChild == nullptr); }
+
+    // Setter for the left child
+    void setLeftChild(BinNode<T> *lc) { leftChild = lc; }
+
+    // Setter for the right child
+    void setRightChild(BinNode<T> *rc) { rightChild = rc; }
 };
-template <typename T>
-BinNode<T>::BinNode(T value) : leftChild(nullptr), rightChild(nullptr)
-{
-    this->value = value;
-}
 
-template <typename T>
-BinNode<T> *BinNode<T>::getLeftChild() const
-{
-    return leftChild;
-}
-
-template <typename T>
-BinNode<T> *BinNode<T>::getRightChild() const
-{
-    return rightChild;
-}
-
-template <typename T>
-T BinNode<T>::getValue()
-{
-    return value;
-}
-
-template <typename T>
-bool BinNode<T>::isLeaf()
-{
-    return (leftChild == nullptr && rightChild == nullptr);
-}
-
-template <typename T>
-void BinNode<T>::setLeftChild(BinNode<T> *lc)
-{
-    leftChild = lc;
-}
-
-template <typename T>
-void BinNode<T>::setRightChild(BinNode<T> *rc)
-{
-    rightChild = rc;
-}
-
+// Define the binary tree traversal template class
 template <typename T>
 class BinTreeTraversal
 {
 private:
-    BinNode<T> *root;
-    void PostfixHelper(BinNode<T> *rootB, string &s)
+    BinNode<T> *root; // Root of the binary tree
+
+    // Helper function for postfix traversal
+    void PostfixHelper(BinNode<T> *node, string &s)
     {
-        if (rootB->getLeftChild() != nullptr)
+        if (node->getLeftChild() != nullptr)
         {
-            PostfixHelper(rootB->getLeftChild(), s);
+            PostfixHelper(node->getLeftChild(), s);
         }
 
-        if (rootB->getRightChild() != nullptr)
+        if (node->getRightChild() != nullptr)
         {
-            PostfixHelper(rootB->getRightChild(), s);
+            PostfixHelper(node->getRightChild(), s);
         }
 
-        s += to_string(rootB->getValue());
+        s += to_string(node->getValue());
     }
 
-    void PrefixHelper(BinNode<T> *rootB, string &s)
+    // Helper function for prefix traversal
+    void PrefixHelper(BinNode<T> *node, string &s)
     {
-        s += to_string(rootB->getValue());
-        if (rootB->getLeftChild() != nullptr)
+        s += to_string(node->getValue());
+
+        if (node->getLeftChild() != nullptr)
         {
-            PostfixHelper(rootB->getLeftChild(), s);
+            PrefixHelper(node->getLeftChild(), s);
         }
 
-        if (rootB->getRightChild() != nullptr)
+        if (node->getRightChild() != nullptr)
         {
-            PostfixHelper(rootB->getRightChild(), s);
+            PrefixHelper(node->getRightChild(), s);
         }
     }
 
-    void InfixHelper(BinNode<T> *rootB, string &s)
+    // Helper function for infix traversal
+    void InfixHelper(BinNode<T> *node, string &s)
     {
-
-        if (rootB->getLeftChild() != nullptr)
+        if (node->getLeftChild() != nullptr)
         {
-            PostfixHelper(rootB->getLeftChild(), s);
+            InfixHelper(node->getLeftChild(), s);
         }
-        s += to_string(rootB->getValue());
-        if (rootB->getRightChild() != nullptr)
+
+        s += to_string(node->getValue());
+
+        if (node->getRightChild() != nullptr)
         {
-            PostfixHelper(rootB->getRightChild(), s);
+            InfixHelper(node->getRightChild(), s);
         }
     }
 
 public:
-    BinTreeTraversal(BinNode<T> *treeRoot);
-    BinNode<T> *getRoot() const;
-    string Infix();
-    string Postfix();
-    string Prefix();
+    // Constructor
+    BinTreeTraversal(BinNode<T> *treeRoot) : root(treeRoot) {}
+
+    // Getter for the root
+    BinNode<T> *getRoot() const { return root; }
+
+    // Function for infix traversal
+    string Infix()
+    {
+        string s = "";
+        InfixHelper(root, s);
+        return s;
+    }
+
+    // Function for postfix traversal
+    string Postfix()
+    {
+        string s = "";
+        PostfixHelper(root, s);
+        return s;
+    }
+
+    // Function for prefix traversal
+    string Prefix()
+    {
+        string s = "";
+        PrefixHelper(root, s);
+        return s;
+    }
 };
-
-template <typename T>
-BinTreeTraversal<T>::BinTreeTraversal(BinNode<T> *treeRoot)
-{
-    root = treeRoot;
-}
-
-template <typename T>
-string BinTreeTraversal<T>::Infix()
-{
-    string s = "";
-    InfixHelper(this->root, s);
-    return s;
-}
-
-template <typename T>
-string BinTreeTraversal<T>::Postfix()
-{
-    string s = "";
-    PostfixHelper(this->root, s);
-    return s;
-}
-
-template <typename T>
-string BinTreeTraversal<T>::Prefix()
-{
-    string s = "";
-    PrefixHelper(this->root, s);
-    return s;
-}
 
 int main(int argc, char const *argv[])
 {
+    // Create a binary tree
     BinNode<int> root(0);
     root.setLeftChild(new BinNode<int>(1));
     root.setRightChild(new BinNode<int>(2));
     root.getLeftChild()->setLeftChild(new BinNode<int>(3));
     root.getLeftChild()->setRightChild(new BinNode<int>(4));
+
+    // Perform tree traversals
     BinTreeTraversal<int> b(&root);
-    cout << b.Infix()<< endl;
-    cout << b.Postfix()<< endl;
-    cout << b.Prefix()<< endl;
+    cout << "Infix: " << b.Infix() << endl;
+    cout << "Postfix: " << b.Postfix() << endl;
+    cout << "Prefix: " << b.Prefix() << endl;
+
     return 0;
 }
